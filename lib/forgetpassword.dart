f06b7e367d9461a55958a59712d2c481b1dfb967
avbigbuddy <136998037+avbigbuddy@users.noreply.github.com>
2024-03-28 11:24:38 +0530

    create forget into page

--- lib/forgetpassword.dart
+++ lib/forgetpassword.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ForgetPass extends StatefulWidget {
   const ForgetPass({super.key});
@@ -25,6 +27,9 @@ class _ForgetPassState extends State<ForgetPass> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          const SizedBox(
+            height: 30,
+          ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(15),
@@ -40,31 +45,140 @@ class _ForgetPassState extends State<ForgetPass> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
+              const Padding(
                 padding: EdgeInsets.all(15),
                 child: Text(
                   "select which contact details should we use to \nreset your password",
                   style: TextStyle(color: Colors.white, fontSize: 18),
                 ),
               ),
-              SizedBox(
-                height: 10,
+              const SizedBox(
+                height: 5,
               ),
               Padding(
                 padding: const EdgeInsets.all(15),
                 child: Container(
-                  height: 75,
+                  height: 70,
                   width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
-                      color: Colors.white,
+                      color: const Color.fromARGB(255, 36, 39, 47),
                       borderRadius: BorderRadius.circular(15),
                       border: Border.all(
                           color: const Color.fromARGB(255, 222, 170, 0),
-                          width: 5)),
+                          width: 3)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Expanded(
+                        child: Center(
+                          child: Icon(
+                            Icons.chat,
+                            size: 30,
+                            color: Color.fromARGB(255, 222, 170, 0),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "via SMS:",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                              Text(
+                                "9878985895",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              )
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, right: 15, left: 15),
+                child: Container(
+                  height: 70,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 36, 39, 47),
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 222, 170, 0),
+                          width: 3)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Expanded(
+                        child: Center(
+                          child: Icon(
+                            Icons.email,
+                            size: 30,
+                            color: Color.fromARGB(255, 222, 170, 0),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: MediaQuery.of(context).size.width,
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "via Email:",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                              Text(
+                                "dtghhjjftytytfytytyry@gmail.com",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
-          ))
+          )),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Container(
+              height: 65,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 212, 175, 55),
+                  borderRadius: BorderRadius.circular(30)),
+              child: Center(
+                child: Text("Continue",
+                    style: GoogleFonts.notoSansThaiLooped(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          )
         ],
       ),
     );
